f640150144d99e6a255c04ddaac2c234cc2f0ee3
Darius Daniel <dariusdaniel771@gmail.com>
2026-02-03 22:43:47 +0100

    Initialize project structure for Launch Countdown Timer challenge with essential files including HTML, CSS, TypeScript, and configuration for ESLint and Vite. Added .gitignore, README, and style guide for project documentation.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex flex-col gap-10">
+      <h1 className="uppercase text-center font-bold text-white text-2xl tracking-[0.25em]">
+        We&apos;re launching soon
+      </h1>
+      <div className="attribution text-white">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://darius-daniel.vercel.app">Darius Daniel</a>.
+      </div>
+    </main>
+  );
+}
+
+export default App;
